22076dc6a21d35054e8da1dbb7eb8419a11f8375
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2024-02-13 08:12:51 +0545

    category crud

--- application/resources/views/admin/category/index.blade.php
+++ application/resources/views/admin/category/index.blade.php
@@ -46,10 +46,20 @@
                                 @endif
                             </td>
                             <td>
-                                <a href="{{ route('admin.categories.edit', $category) }}" class="btn btn-primary btn-sm">Edit</a>
-                                <a href="{{ route('admin.categories.destroy', $category) }}"
-                                    class="btn btn-danger btn-sm delete_row" data-toggle="modal"
-                                    data-target="#deleteModal">Delete</a>
+                                <ul class="nav ">
+                                    <li class="nav-item">
+                                        <a class="nav-link text-dark dropdown-toggle" data-toggle="dropdown" href="#"
+                                            role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-h fa-2x"></i></a>
+                                        <div class="dropdown-menu">
+                                            <a href="{{ route('admin.categories.edit', $category) }}"
+                                            class="text-dark dropdown-item">Edit</a>
+                                            <a href="{{ route('admin.categories.edit', $category) }}"
+                                                    class="text-dark dropdown-item delete_row" data-bs-toggle="modal"
+                                                    data-bs-target="#deleteModal">Delete</a>
+
+                                        </div>
+                                    </li>
+                                    </ul>
 
                             </td>
 

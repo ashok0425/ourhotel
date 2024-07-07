d838c851e3840f3497526a7e0c4a64e0d8f50a32
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2024-07-07 13:01:35 +0545

    updated

--- app/Http/Controllers/API/PlaceController.php
+++ app/Http/Controllers/API/PlaceController.php
@@ -217,7 +217,7 @@ class PlaceController extends Controller
         ->orderBy('price', 'asc')
         ->where("( 6371 * acos( cos( radians(?) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(?) ) + sin( radians(?) ) * sin( radians( latitude ) ) ) ) AS distance", [$latitude, $longitude, $latitude])
         ->having('distance', '<=', 2)
-        ->limit(10);
+        ->limit(10)->get();
 
         $data = [
             'places' => $places

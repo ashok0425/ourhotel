68e8de79e2471e8c568a3e83ef5682c82cb0bda9
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2024-07-12 15:38:59 +0545

    update sentry path

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Sentry\Laravel\Features\Storage\Integration;
+use Sentry\Laravel\Integration;
 use Throwable;
 
 class Handler extends ExceptionHandler

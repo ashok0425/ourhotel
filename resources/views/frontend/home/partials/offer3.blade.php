0ac75f97fb634f50860ffa63f0e8866246bf2d80
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2024-07-24 09:20:09 +0545

    change offer value

--- resources/views/frontend/home/partials/offer3.blade.php
+++ resources/views/frontend/home/partials/offer3.blade.php
@@ -1,12 +1,12 @@
 <div class="container my-5">
     <div class="">
         @php
-            $offer1=coupons()->skip(3)->take(1)->first();
+            $offer4=coupons()->skip(3)->take(1)->first();
         @endphp
-        @if ($offer1)
+        @if ($offer4)
             <div class="offer-wrapper">
-               <a href="{{$offer1->link}}">
-                <img lsrc="{{getImageUrl($offer1->thumbnail)}}" alt="NSN offer" class="custom-border-radius-20 w-100" loading='lazy'>
+               <a href="{{$offer4->link}}">
+                <img lsrc="{{getImageUrl($offer4->thumbnail)}}" alt="NSN offer" class="custom-border-radius-20 w-100" loading='lazy'>
                </a>
             </div>
         @endif
--- resources/views/frontend/home/partials/offer4.blade.php
+++ resources/views/frontend/home/partials/offer4.blade.php
@@ -1,24 +1,24 @@
 <div class="container">
     <div class="row">
         @php
-            $offer2=DB::table('coupon')->skip(4)->take(1)->first();
-            $offer3=DB::table('coupon')->skip(5)->take(1)->first();
+            $offer5=DB::table('coupon')->skip(4)->take(1)->first();
+            $offer6=DB::table('coupon')->skip(5)->take(1)->first();
 
         @endphp
        <div class="col-md-6">
-        @if ($offer2)
+        @if ($offer5)
         <div class="offer-wrapper mb-4">
-           <a href="{{$offer2->link}}">
-            <img lsrc="{{getImageUrl($offer2->thumb)}}" alt="NSN offer" class="custom-border-radius-20 w-100">
+           <a href="{{$offer5->link}}">
+            <img lsrc="{{getImageUrl($offer5->thumb)}}" alt="NSN offer" class="custom-border-radius-20 w-100">
            </a>
         </div>
     @endif
        </div>
        <div class="col-md-6">
-        @if ($offer3)
+        @if ($offer6)
         <div class="offer-wrapper mb-4">
-           <a href="{{$offer3->link}}">
-            <img lsrc="{{getImageUrl($offer3->thumb)}}" alt="NSN offer" class="custom-border-radius-20 w-100">
+           <a href="{{$offer6->link}}">
+            <img lsrc="{{getImageUrl($offer6->thumb)}}" alt="NSN offer" class="custom-border-radius-20 w-100">
            </a>
         </div>
     @endif
